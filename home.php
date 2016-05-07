43b04f002136751039f068ca55d7eedb88da5ed6
tungpun <htung.nht@gmail.com>
2016-05-07 11:21:17 +0700

    smt

--- home.php
+++ home.php
@@ -110,9 +110,9 @@
 			 	<p id="errorMessage" class="hidden"></p>
 			</div>
 		</div>
-		<div id="footer">
+		<div id="footer">			
 			<div class="container">
-			<p>Powered by Guru Team<span class="font-grey-4">|</span> <a href="contact">Contact</a> <span class="font-grey-4">|</span> <a href="donate">Donate</a> 
+			<p>Powered by Guru Team<span class="font-grey-4">|</span> <a href="register">Đăng ký kiểm tra trạng thái website</a><span class="font-grey-4">|</span> <a href="contact">Contact</a> <span class="font-grey-4">|</span> <a href="donate">Donate</a> 
 			</p>
 			</div>
 			<a href="#" class="gototop"><i class="icon-arrow-up"></i></a>
