aef8417976ed74a8c72a000a1adab3d3f9786451
tungpun <htung.nht@gmail.com>
2016-05-09 22:45:48 +0700

    fix bug check website

--- check_website_status.py
+++ check_website_status.py
@@ -30,7 +30,12 @@ def notify(web, status, r):
         message = "Chao " + name + ",\r\nGuruWS xin thong bao:\nTrang web " + url + " hien khong the truy cap duoc vao thoi diem " + datatime + "\r\n"
     else:
         message = "Chao " + name + ",\r\nGuruWS xin thong bao:\nTrang web " + url + " hien da co the truy cap duoc vao thoi diem " + datatime + "\r\n"    
-    message += "Chi tiet:\r\nGET " + url + " (" + str(r.status_code) + ")" + "\r\n" + str(r.headers)
+        
+    try:
+        message += "Chi tiet:\r\nGET " + url + " (" + str(r.status_code) + ")" + "\r\n" + str(r.headers)
+    except Exception, e:
+        pass
+
     message += "\r\n\r\n\r\n--\r\nBan nhan duoc thu nay vi da dang ky cap nhat trang thai Website tai guruws.tech.\r\nCam on ban da su dung dich vu\r\nNeu can ho tro gi them (vi du nhu khac phuc su co, tim kiem ho hong website) cac ban co the lien he voi chung toi qua email htung.nht@gmail.com hoac duong day nong: 01646543714\r\nGuruTeam"
  
     try:
@@ -38,6 +43,7 @@ def notify(web, status, r):
         msg = MIMEMultipart()
         msg['From'] = "Guru Team"
         msg['To'] = toemail
+
         if status == 'down':
             msg['Subject'] = "GuruWS: " + url + " khong truy cap duoc"
         else:
@@ -118,7 +124,7 @@ def check_website_status():
                 up = False
         except Exception, e:
             #print "[+] Error ! " + str(e)
-            up = False
+            up = False            
             pass
 
         if up:
