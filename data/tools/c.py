b9cc7ded7723449b23d37b4bf561a973eff8457b
tungpun <htung.nht@gmail.com>
2017-05-12 08:53:08 -0700

    updated data

--- data/tools/c.py
+++ data/tools/c.py
@@ -1,7 +1,7 @@
 import requests
 import re
 import os
-
+import hashlib
 
 for i in range (1, 3):
     url = "https://github.com/search?utf8=%E2%9C%93&q=plugin+wordpress+language%3APHP&type=Repositories&ref=advsearch&l=PHP&l=&p={0}".format(i)
@@ -9,11 +9,15 @@ for i in range (1, 3):
     searchResult = req.text
     searchResultWithoutEndline = searchResult.replace("\n", "")
     # print searchResultWithoutEndline
-    matchList = re.findall(r"<h3 class=\"repo-list-name\">    <a href=\"(.*?)</a>      </h3>", searchResultWithoutEndline, flags=0)
+    matchList = re.findall(r' <a class="muted-link" href="(.*?)">', searchResultWithoutEndline, flags=0)
     if matchList:
         for project in matchList:
-           projectDownloadURL = "https://github.com/" + re.sub(r"\">.*", "", project) + "/archive/master.zip"
-           os.system("wget " + projectDownloadURL)
+            filename = re.sub(r"\">.*", "", project)[:-10]
+            if filename[-1] == '/':
+                filename = filename[:-1]
+            projectDownloadURL = "https://github.com/" + filename + "/archive/master.zip"
+            print projectDownloadURL
+            os.system("wget " + projectDownloadURL + " -O " + hashlib.md5(filename).hexdigest() + ".zip")
     else:
         print "Not found in page" + str(i)
         continue
